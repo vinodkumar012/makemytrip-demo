ac22012a5371ea24db1499520ff21f01ce6ec802
vinod <b.vinodkumar012@gmail.com>
2017-01-29 19:36:03 +0530

    Added sl4j dependency for logging

--- src/main/java/com/makemytrip/driver/DriverManager.java
+++ src/main/java/com/makemytrip/driver/DriverManager.java
@@ -9,10 +9,15 @@ import java.net.URL;
 
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DriverManager {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DriverManager.class);
+	
 	public static AppiumDriver getDriver(String platformName, String browserName, String version, String appPackage, String appActivity, String deviceName, String apkFileName, String nodeURL) throws MalformedURLException {
+		LOGGER.info("Going to initiate driver");
 		DesiredCapabilities androidCapabilities = new DesiredCapabilities();
 		androidCapabilities.setCapability("platformName", platformName);
 		androidCapabilities.setCapability(CapabilityType.BROWSER_NAME, browserName);
@@ -20,9 +25,11 @@ public class DriverManager {
 		androidCapabilities.setCapability("appPackage", appPackage);
 		androidCapabilities.setCapability("appActivity", appActivity);
 		androidCapabilities.setCapability("deviceName", deviceName);
-		File baseApkPath = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator);
-		androidCapabilities.setCapability("app", new File(baseApkPath + apkFileName).getAbsolutePath());
-		return new AndroidDriver(new URL(nodeURL),androidCapabilities);
+		File baseApkPath = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "resources");
+		androidCapabilities.setCapability("app", new File(baseApkPath + File.separator + apkFileName).getAbsolutePath());
+		AppiumDriver driver = new AndroidDriver(new URL(nodeURL),androidCapabilities);
+		LOGGER.info("Initiated driver with capabilities as : " + androidCapabilities);
+		return driver;
 	}
 
 }
